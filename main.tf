d520dad50476a434db236b82e10e1f18b7e4315c
Ahsan Ahmed <117782347+Ahsan22812@users.noreply.github.com>
2024-03-26 15:04:40 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+backend "s3" {
+    bucket = "terraform-aws-bucket-01"
+    key    = "path/to/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+
  # profile = "terraform-user"
 }
 
